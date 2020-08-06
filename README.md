a162d55d4b3c760dca3a224e3b4ddc2bb25b0e17
Julien Vernay <julien.vernay@telecom-st-etienne.fr>
2020-08-06 12:24:06 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# `utf8proc` library - Build2 package
+
+`utf8proc` is a small, clean C library that provides Unicode normalization, case-folding, and other operations for data in the UTF-8 encoding.
+
+This repository contains a packaging of `utf8proc` library for the **Build2** build system.
+
+See also:
+
+- `utf8proc`: https://github.com/JuliaStrings/utf8proc
+- **Build2**: https://build2.org
+
---
+++ pull-files.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+echo "Hello world"
+
+function pushdd { 
+  pushd $1 > /dev/null
+}
+function popdd {
+  popd > /dev/null
+}
+
+baseurl="https://raw.githubusercontent.com/JuliaStrings/utf8proc/master"
+
+pushdd libutf8proc
+  rm utf8proc*
+  wget "$baseurl/utf8proc.h"
+  wget "$baseurl/utf8proc.c"
+  wget "$baseurl/utf8proc_data.c"
+popdd
+
+cp libutf8proc/utf8proc.h tests/
+
+pushdd tests
+  wget "$baseurl/test/tests.c"
+  wget "$baseurl/test/tests.h"
+
+  for testcase in case charwidth custom iterate misc printproperty valid
+  do
+    rm -rf $testcase
+    mkdir $testcase
+    pushdd $testcase
+      cp ../testcase_buildfile buildfile
+      cp ../tests.c tests.c
+      cp ../tests.h tests.h
+      wget "$baseurl/test/${testcase}.c" -O driver.c
+    popdd
+  done
+
+  rm tests.c tests.h
+popdd
+
+pwd
